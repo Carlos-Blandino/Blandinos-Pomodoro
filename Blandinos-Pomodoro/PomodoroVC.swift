887fa2d75256e40611255d58768bf63870cdcc58
Carlos Blandino <cablandino66@gmail.com>
2020-05-16 20:17:27 -0700

    setting up initial design

--- Blandinos-Pomodoro/PomodoroVC.swift
+++ Blandinos-Pomodoro/PomodoroVC.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PomodoroVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
 }
 
